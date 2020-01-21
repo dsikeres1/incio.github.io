f51f33633b5e7d368ff7967d78e536b045995d1d
Incio <inciojs@gmail.com>
2020-01-22 01:48:10 +0900

    category pagination wip

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,7 +10,7 @@ $('.-sidebar-group-link').on('click', function () {
     }
 });
 
-$('pre > code').each(function() {
+$('pre > code').each(function () {
     let lines = this.innerHTML.split("\n");
     let ol = '<ol>';
 
@@ -36,8 +36,9 @@ SimpleJekyllSearch({
 $('html, body').on('mousewheel DOMMouseScroll', function (e) {
     scrollFunction();
 });
+
 function scrollFunction() {
-    let _scrollTop = document.body.scrollTop  || document.documentElement.scrollTop || window.pageYOffset || $('.-container').scrollTop();
+    let _scrollTop = document.body.scrollTop || document.documentElement.scrollTop || window.pageYOffset || $('.-container').scrollTop();
     let _windowWidth = $(window).width();
 
     if (_windowWidth > 783) {
@@ -47,10 +48,10 @@ function scrollFunction() {
         const cut = 90;
         // pc
         if (_scrollTop <= cut) {
-            $('.-top-header').css('padding-top', _minPadding - _scrollTop / cut  * 23 + 'px');
-            $('.-top-header').css('padding-bottom', _minPadding - _scrollTop / cut  * 23 + 'px');
-            $('.-top-header-title').css('fontSize', _minTitleFontSize - _scrollTop/ cut / 10 * 5 + 'rem' );
-            $('.-top-header-meta').css('fontSize', _minMetaFontSize - _scrollTop/ cut / 10 * 7 + 'rem');
+            $('.-top-header').css('padding-top', _minPadding - _scrollTop / cut * 23 + 'px');
+            $('.-top-header').css('padding-bottom', _minPadding - _scrollTop / cut * 23 + 'px');
+            $('.-top-header-title').css('fontSize', _minTitleFontSize - _scrollTop / cut / 10 * 5 + 'rem');
+            $('.-top-header-meta').css('fontSize', _minMetaFontSize - _scrollTop / cut / 10 * 7 + 'rem');
         } else if (_scrollTop > cut) {
             $('.-top-header').css('padding-top', '7px');
             $('.-top-header').css('padding-bottom', '7px');
@@ -65,21 +66,70 @@ function scrollFunction() {
     }
 }
 
-$( document ).ready(function() {
-    const _category_page_cnt = parseInt($('.-category-posts-list li').length / 5);
+const _category_page_cnt = parseInt($('.-category-posts-list li').length / 5) + 1;
+$(document).ready(function () {
 
-    $("#ul_id").append("<li>pre</li>");
+    $(".-category-pagination").append('<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true" data-page="-">Previous</a></li>');
 
-    for (i = 1; i <= _category_page_cnt + 1; i++) {
-        $("#ul_id").append("<li>i</li>");
+    for (i = 1; i <= _category_page_cnt; i++) {
+        $(".-category-pagination").append('<li class="page-item"><a class="page-link" href="#" onclick="categoryPagination(' + i + ');">' + i + '</a></li>');
     }
 
-    $("#ul_id").append("<li>next</li>");
-
+    $(".-category-pagination").append('<li class="page-item"><a class="page-link" href="#" data-page="+">Next</a></li>');
+    categoryPagination(1);
 });
 
-console.log();
 
-$('.-category-posts-list').find('li').each(function(i, e){
-    console.log($(this).text());
-});
+function categoryPagination(page) {
+    $('.-category-posts-list').find('li').each(function (i, e) {
+        if (page * 5 - 5 <= i && i < page * 5) {
+            $(this).addClass("d-block").removeClass("d-none");
+        } else {
+            $(this).addClass("d-none").removeClass("d-block");
+        }
+    });
+
+    $('.-category-posts-list').find('hr').each(function (i, e) {
+        if (page * 5 - 5 <= i && i < page * 5) {
+            $(this).addClass("d-block").removeClass("d-none");
+        } else {
+            $(this).addClass("d-none").removeClass("d-block");
+        }
+    });
+
+    $('.-category-pagination').find('li').each(function (i, e) {
+        if (page === 1) {
+            if (page === i) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+
+            if ($('.-category-pagination li').length > 3) {
+                $('.-category-pagination li:eq(0)').addClass("disabled");
+                $('.-category-pagination li:eq(last)').removeClass("disabled");
+            } else {
+                $('.-category-pagination li:eq(0)').addClass("disabled");
+                $('.-category-pagination li:eq(last)').addClass("disabled");
+            }
+        } else if (page === _category_page_cnt) {
+            if (page === i) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+            if ($('.-category-pagination li').length > 3) {
+                $('.-category-pagination li:eq(last)').addClass("disabled");
+                $('.-category-pagination li:eq(0)').removeClass("disabled");
+            } else {
+                $('.-category-pagination li:eq(0)').addClass("disabled");
+                $('.-category-pagination li:eq(last)').addClass("disabled");
+            }
+        } else if (page === i) {
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+
+}
