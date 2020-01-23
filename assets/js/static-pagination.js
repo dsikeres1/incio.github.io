0f7f2b4d38e8d39e6108ebd1088eb37c69483897
Incio <inciojs@gmail.com>
2020-01-23 11:45:01 +0900

    static-pagination.js function name pagination -> staticPagination on static-pagination.js

--- assets/js/static-pagination.js
+++ assets/js/static-pagination.js
@@ -1,6 +1,15 @@
+/*
+    title: static-pagination.js
+    description: Pagination for static pages based on bootstrap and jquery (eg. jekyll)
+    author: Incio
+    github: https://github.com/inci-o
+    site: https://inci-o/github.io
+    version: 0.1.0
+ */
+
 let _currentPage, $category_list;
 
-function pagination(perPage, page, category_list) {
+function staticPagination(perPage, page, category_list) {
     let $list = $('#static-pagination-list');
     let $pagination = $('#static-pagination');
     if (category_list !== null) {
@@ -9,10 +18,10 @@ function pagination(perPage, page, category_list) {
     const _paginationLength = parseInt($category_list.length / perPage) + 1;
     if (page === 0 && _currentPage !== 1) {
         // 이전
-        pagination(5, _currentPage - 1, null);
+        staticPagination(5, _currentPage - 1, null);
     } else if (page === _paginationLength + 1 && _currentPage !== _paginationLength + 1) {
         // 다음
-        pagination(5, _currentPage + 1, null);
+        staticPagination(5, _currentPage + 1, null);
     } else {
         // 일반 선택
         _currentPage = page;
@@ -35,13 +44,13 @@ function pagination(perPage, page, category_list) {
         $pagination.empty();
         if (_currentPage === 1) {
             $pagination.append('<li class="page-item disabled">' +
-                '<a class="page-link" href="#" onclick="pagination(' + perPage + ', ' + 0 + ', ' + null + ');">' +
+                '<a class="page-link" href="#" onclick="staticPagination(' + perPage + ', ' + 0 + ', ' + null + ');">' +
                 '<i class="material-icons" style="font-size: 14px;">arrow_back_ios</i>' +
                 '</a>' +
                 '</li>');
         } else {
             $pagination.append('<li class="page-item">' +
-                '<a class="page-link" href="#" onclick="pagination(' + perPage + ', ' + 0 + ', ' + null + ');">' +
+                '<a class="page-link" href="#" onclick="staticPagination(' + perPage + ', ' + 0 + ', ' + null + ');">' +
                 '<i class="material-icons" style="font-size: 14px;">arrow_back_ios</i>' +
                 '</a>' +
                 '</li>');
@@ -50,24 +59,24 @@ function pagination(perPage, page, category_list) {
         for (let _page = 1; _page <= _paginationLength; _page++) {
             if (_page === _currentPage) {
                 $pagination.append('<li class="page-item">' +
-                    '<a class="page-link active" href="#" onclick="pagination(' + perPage + ', ' + _page + ', ' + null + ');">' + _page + '</a>' +
+                    '<a class="page-link active" href="#" onclick="staticPagination(' + perPage + ', ' + _page + ', ' + null + ');">' + _page + '</a>' +
                     '</li>');
             } else {
                 $pagination.append('<li class="page-item">' +
-                    '<a class="page-link" href="#" onclick="pagination(' + perPage + ', ' + _page + ', ' + null + ');">' + _page + '</a>' +
+                    '<a class="page-link" href="#" onclick="staticPagination(' + perPage + ', ' + _page + ', ' + null + ');">' + _page + '</a>' +
                     '</li>');
             }
         }
 
         if (_currentPage === _paginationLength) {
             $pagination.append('<li class="page-item disabled">' +
-                '<a class="page-link" href="#" onclick="pagination(' + perPage + ', ' + (_paginationLength + 1) + ', ' + null + ');">' +
+                '<a class="page-link" href="#" onclick="staticPagination(' + perPage + ', ' + (_paginationLength + 1) + ', ' + null + ');">' +
                 '<i class="material-icons" style="font-size: 14px;">arrow_forward_ios</i>' +
                 '</a>' +
                 '</li>');
         } else {
             $pagination.append('<li class="page-item">' +
-                '<a class="page-link" href="#" onclick="pagination(' + perPage + ', ' + (_paginationLength + 1) + ', ' + null + ');">' +
+                '<a class="page-link" href="#" onclick="staticPagination(' + perPage + ', ' + (_paginationLength + 1) + ', ' + null + ');">' +
                 '<i class="material-icons" style="font-size: 14px;">arrow_forward_ios</i>' +
                 '</a>' +
                 '</li>');
